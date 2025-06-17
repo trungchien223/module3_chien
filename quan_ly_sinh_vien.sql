0a050f94864652f562def0c99a30b440f2693a71
TRAN TRUNG CHIEN <you@example.com>
2025-06-17 11:50:45 +0700

    nop bai tap

---
+++ quan_ly_sinh_vien.sql
@@ -0,0 +1,18 @@
+create database demo;
+use demo;
+create table Student(
+id int,
+name varchar(200),
+age int,
+country varchar(50)
+);
+create table class(
+	id int,
+    name varchar(30)
+    );
+    create table teacher(
+    id int,
+    name varchar(30),
+    age int,
+    country varchar(50)
+    );
